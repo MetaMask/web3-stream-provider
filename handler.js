5f5b841257791cee60ca14677adc2dde3e08587a
kumavis <aaron@kumavis.me>
2016-05-22 15:05:15 -0700

    pull in fixes from metamask-extension

--- handler.js
+++ handler.js
@@ -3,52 +3,23 @@ const inherits = require('util').inherits
 
 module.exports = StreamHandler
 
-
-inherits(StreamHandler, Duplex)
-
-function StreamHandler(provider){
-  Duplex.call(this, {
-    objectMode: true,
-  })
-
-  this.provider = provider
-}
-
-// private
-
-StreamHandler.prototype._onMessage = function(payload){
-  const self = this
-  self.provider.sendAsync(payload, function(err, result){
-    // convert errors into RPC errors
-    if (err) {
-      if (Array.isArray(payload)) {
-        result = payload.map(convertPayloadToError.bind(null, err))
-      } else {
-        result = convertPayloadToError(err, payload)
+function handleRequestsFromStream(stream, provider, logger){
+  logger = logger || noop
+  
+  stream.on('data', function onRpcRequest(payload){
+    provider.sendAsync(payload, function onPayloadHandled(err, response){
+      logger(null, payload, response)
+      try {
+        stream.write(response)
+      } catch (err) {
+        logger(err)
       }
-    }
-    
-    self.push(result)
+    })
   })
+
 }
 
-// stream plumbing
-
-StreamHandler.prototype._read = noop
-
-StreamHandler.prototype._write = function(msg, encoding, cb){
-  this._onMessage(msg)
-  cb()
-}
 
 // util
 
 function noop(){}
-
-function convertPayloadToError(err, payload){
-  return {
-    id: payload.id,
-    jsonrpc: payload.jsonrpc,
-    error: err.stack,
-  }
-}
--- index.js
+++ index.js
@@ -17,31 +17,43 @@ function StreamProvider(){
 // public
 
 StreamProvider.prototype.send = function(payload){
-  throw new Error('StreamProvider - does not support synchronous RPC calls')
+  throw new Error('StreamProvider - does not support synchronous RPC calls. called: "'+payload.method+'"')
 }
 
 StreamProvider.prototype.sendAsync = function(payload, callback){
   // console.log('StreamProvider - sending payload', payload)
-  this._payloads[payload.id] = [payload, callback]
+  var id = payload.id
+  if (Array.isArray(payload)) {
+    id = 'batch'+payload[0].id
+  }
+  this._payloads[id] = [payload, callback]
   // console.log('payload for plugin:', payload)
   this.push(payload)
+}
+
+StreamProvider.prototype.isConnected = function(){
+  return true
 }
 
 // private
 
 StreamProvider.prototype._onResponse = function(response){
   // console.log('StreamProvider - got response', payload)
-  var data = this._payloads[response.id]
+  var id = response.id
+  if (Array.isArray(response)) {
+    id = 'batch'+response[0].id
+  }
+  var data = this._payloads[id]
   if (!data) throw new Error('StreamProvider - Unknown response id')
-  delete this._payloads[response.id]
+  delete this._payloads[id]
   var payload = data[0]
   var callback = data[1]
 
   // logging
-  var res = Array.isArray(response) ? response : [response]
-  ;(Array.isArray(payload) ? payload : [payload]).forEach(function(payload, index){
-    console.log('plugin response:', payload.id, payload.method, payload.params, '->', res[index].result)
-  })
+  // var res = Array.isArray(response) ? response : [response]
+  // ;(Array.isArray(payload) ? payload : [payload]).forEach(function(payload, index){
+  //   console.log('plugin response:', payload.id, payload.method, payload.params, '->', res[index].result)
+  // })
 
   callback(null, response)
 }
