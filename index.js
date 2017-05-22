edcc8be1e07e493b73138038aa7f9af4412264e0
Dan Finlay <dan@danfinlay.com>
2017-05-22 14:52:30 -0700

    Inherit stream provider from provider engine

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const ProviderEngine = require('web3-provider-engine')
 const StreamSubprovider = require('./stream-subprovider')
 const inherits = require('inherits')
 
-module.exports = StreamProvider
+inherits(StreamProvider, ProviderEngine)
 
+function StreamProvider () {
+  ProviderEngine.call(this)
 
-function StreamProvider(){
-  const engine = new ProviderEngine()
-
-  const streamSubprovider = new StreamSubprovider()
-  engine.addProvider(streamSubprovider)
-
-  // start polling
-  engine.start()
-
-  return engine
+  const streamSubprovider = this.stream = new StreamSubprovider()
+  this.addProvider(streamSubprovider)
+  this.start()
 }
+
+
+module.exports = StreamProvider
 
